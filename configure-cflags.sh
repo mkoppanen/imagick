0932a954e202464fafa1bba682ba8953fe6c4ae4
Danack <Danack@basereality.com>
2018-12-02 01:39:04 +0000

    Tweaking CFLAGS.

--- configure-cflags.sh
+++ configure-cflags.sh
@@ -24,39 +24,48 @@ function contains() {
     return 0
 }
 
-
-
-strictPHPVersions=()
-strictPHPVersions+=("5.6")
-strictPHPVersions+=("7")
-strictPHPVersions+=("7.0")
-strictPHPVersions+=("7.1")
-
-strictImageMagickVersions=()
-strictImageMagickVersions+=("dev")
-strictImageMagickVersions+=("6.8.7-0")
-strictImageMagickVersions+=("6.7.5-3")
-strictImageMagickVersions+=("6.6.0-9")
-strictImageMagickVersions+=("7.0.8-4")
-strictImageMagickVersions+=("7.0.1-0")
+#
+#superWeakPHPVersions=()
+#superWeakPHPVersions+=("5.4")
+#superWeakPHPVersions+=("5.5")
+#
+#weakPHPVersions=()
+#weakPHPVersions+=("5.6")
+#
+##strictPHPVersions+=("7")
+##strictPHPVersions+=("7.0")
+##strictPHPVersions+=("7.1")
+#
+#weakImageMagickVersions=()
+##strictImageMagickVersions+=("dev")
+##strictImageMagickVersions+=("6.8.7-0")
+##strictImageMagickVersions+=("6.7.5-3")
+##strictImageMagickVersions+=("6.6.0-9")
+##strictImageMagickVersions+=("7.0.8-4")
+##strictImageMagickVersions+=("7.0.1-0")
 
 
 echo "TRAVIS_PHP_VERSION is ${TRAVIS_PHP_VERSION}"
 echo "IMAGEMAGICK_VERSION is ${IMAGEMAGICK_VERSION}"
 
-strictImageMagick=$(contains "${strictImageMagickVersions[@]}" "${IMAGEMAGICK_VERSION}" )
-strictPHP=$(contains "${strictPHPVersions[@]}" "${TRAVIS_PHP_VERSION}" )
+#weakImageMagick=$(contains "${weakImageMagickVersions[@]}" "${IMAGEMAGICK_VERSION}" )
+#superWeakPHP=$(contains "${superWeakPHPVersions[@]}" "${TRAVIS_PHP_VERSION}" )
+#weakPHP=$(contains "${weakPHPVersions[@]}" "${TRAVIS_PHP_VERSION}" )
+#
+#echo "weakImageMagick is ${weakImageMagick}"
+#echo "superWeakPHPVersion is ${superWeakPHPVersion}"
+#echo "weakPHP is ${weakPHP}"
 
-echo "strictImageMagick is ${strictImageMagick}"
-echo "strictPHP is ${strictPHP}"
 
 # TODO - test if this flag is usable for the tests on all appropriate platforms
 # -Wmaybe-uninitialized
 
-if [[ $strictPHP = '1' ]] && [[ $strictImageMagick = '1' ]]; then 
-	CFLAGS="-Wno-deprecated-declarations -Wdeclaration-after-statement -Werror -Wall -Wextra -Wimplicit-function-declaration";
-else 
+if [[ "${TRAVIS_PHP_VERSION}" == "5.4" || "${TRAVIS_PHP_VERSION}" == "5.5"  ]]; then
 	CFLAGS="-Wno-deprecated-declarations";
+else if [[ "${TRAVIS_PHP_VERSION}" == "5.6" ]]; then
+	CFLAGS="-Wno-deprecated-declarations -Wdeclaration-after-statement -Werror -Wall -Wimplicit-function-declaration";
+else
+    CFLAGS="-Wno-deprecated-declarations -Wdeclaration-after-statement -Werror -Wall -Wextra -Wimplicit-function-declaration";
 fi
 
 echo "Setting CFLAGS to ${CFLAGS}"
