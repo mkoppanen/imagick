626b18427dc3958cf03c7f75c27a91bc3c11fbc2
Danack <Danack@basereality.com>
2021-04-29 09:33:42 +0100

    Fix small 'developing' container problems.

--- docker/installPHP.sh
+++ docker/installPHP.sh
@@ -16,7 +16,9 @@ cd /var/app/php_src/${SRCFILE}
 
 # ./buildconf
 # ./configure --disable-all --without-pear
-./configure --without-pear
+./configure --without-pear --without-sqlite3 --without-pdo-sqlite --with-openssl --enable-mbstring
+
+
 
 
 make -j 4
